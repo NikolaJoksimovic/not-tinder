308e1ee0667912f85216d605b656a3b810aeef51
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-15 21:54:15 +0200

    add onboarding page, styled and everything

--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -3,128 +3,198 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const OnBoarding = () => {
+  const [checkBoxes, setCheckBoxes] = useState({
+    my: false, // [m]an_gender-identit[y]
+    wy: false,
+    mt: false, // [m]an_gender-interes[t]
+    wt: false,
+  });
+  const [formData, setFormData] = useState({
+    user_id: "",
+    first_name: "",
+    dob_day: "",
+    dob_month: "",
+    dob_year: "",
+    show_gender: "false",
+    gender_identity: "",
+    gender_interest: "",
+    email: "",
+    url: "",
+    about: "",
+    matches: [],
+  });
+
   const authorizationToken = true;
   const handleSubmit = (e) => {};
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.type === "raido" ? e.target.checked : e.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleRadioClick = (e) => {
+    const id = e.target.id;
+    const firstLetter = id.substring(0, 1);
+    const secondLetter = id.substring(id.length - 1, id.length);
+    const name = id.substring(0, 1) + id.substring(id.length - 1, id.length);
+    let newCheckBoxes =
+      secondLetter === "y"
+        ? { ...checkBoxes, ["my"]: false, ["wy"]: false }
+        : { ...checkBoxes, ["mt"]: false, ["wt"]: false };
+    setCheckBoxes({ ...newCheckBoxes, [name]: true });
+  };
   return (
     <>
       <Navbar setShowModal={() => {}} showModal={false}></Navbar>
-      <div className='onboarding'>
+      <div className='onboarding center-flex-column height-800'>
         <h2>create account</h2>
         <form action='' onSubmit={handleSubmit}>
-          <section>
-            <label htmlFor='first_name'>FirstName</label>
-            <input
-              id='first_name'
-              type='text'
-              name='first_name'
-              placeholder='First Name'
-              required={true}
-              onChange={handleChange}
-            />
-          </section>
-          <section>
-            <label>Birthday</label>
-            <div className='dob_inputs'>
-              <input
-                id='dob_day'
-                type='number'
-                name='dob_day'
-                placeholder='DD'
-                required={true}
-                onChange={handleChange}
-              />
-              <input
-                id='dob_month'
-                type='number'
-                name='dob_month'
-                placeholder='MM'
-                required={true}
-                onChange={handleChange}
-              />
-              <input
-                id='dob_year'
-                type='number'
-                name='dob_year'
-                placeholder='YYYY'
-                required={true}
-                onChange={handleChange}
-              />
+          <div className='screen-md'>
+            <div className='screen-md-left'>
+              <section>
+                <label htmlFor='first_name'>FirstName</label>
+                <input
+                  className='first-name-input'
+                  id='first_name'
+                  type='text'
+                  name='first_name'
+                  placeholder='First Name'
+                  required={true}
+                  value={formData.first_name}
+                  onChange={handleChange}
+                />
+              </section>{" "}
+              <section>
+                <label>Birthday</label>
+                <div className='dob_inputs center-flex-row'>
+                  <input
+                    className='dob-input'
+                    id='dob_day'
+                    type='number'
+                    name='dob_day'
+                    placeholder='DD'
+                    required={true}
+                    value={formData.dob_day}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className='dob-input'
+                    id='dob_month'
+                    type='number'
+                    name='dob_month'
+                    placeholder='MM'
+                    required={true}
+                    value={formData.dob_month}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className='dob-input'
+                    id='dob_year'
+                    type='number'
+                    name='dob_year'
+                    placeholder='YYYY'
+                    required={true}
+                    value={formData.dob_year}
+                    onChange={handleChange}
+                  />
+                </div>
+              </section>
+              <section>
+                <label htmlFor=''>Gender</label>
+                <div className='gender-inputs center-flex-row'>
+                  <label htmlFor='man-gender-identity'>Man</label>
+                  <input
+                    className='gender-input'
+                    id='man-gender-identity'
+                    type='radio'
+                    name='gender_identity'
+                    required={true}
+                    value='man'
+                    onChange={handleChange}
+                    onClick={handleRadioClick}
+                    checked={checkBoxes.my}
+                  />
+                  <label htmlFor='woman-gender-identity'>Woman</label>
+                  <input
+                    className='gender-input'
+                    id='woman-gender-identity'
+                    type='radio'
+                    name='gender_identity'
+                    required={true}
+                    value='woman'
+                    onChange={handleChange}
+                    onClick={handleRadioClick}
+                    checked={checkBoxes.wy}
+                  />
+                </div>
+              </section>
+              <section>
+                <label htmlFor=''>Show me</label>
+                <div className='interest-inputs'>
+                  <label htmlFor='man-gender-interest'>Man</label>
+                  <input
+                    id='man-gender-interest'
+                    type='radio'
+                    name='gender_interest'
+                    required={true}
+                    value='man'
+                    onChange={handleChange}
+                    onClick={handleRadioClick}
+                    checked={checkBoxes.mt}
+                  />
+                  <label htmlFor='woman-gender-interest'>Woman</label>
+                  <input
+                    id='woman-gender-interest'
+                    type='radio'
+                    name='gender_interest'
+                    required={true}
+                    value='woman'
+                    onChange={handleChange}
+                    onClick={handleRadioClick}
+                    checked={checkBoxes.wt}
+                  />
+                </div>
+              </section>
+              <section>
+                <label htmlFor='about'>About us(me)</label>
+                <input
+                  className='about-us-input'
+                  id='about'
+                  type='text'
+                  name='about'
+                  required={true}
+                  placeholder='I like long walks...'
+                  value={formData.about}
+                  onChange={handleChange}
+                />
+              </section>
             </div>
-          </section>
-          <section>
-            <label htmlFor=''>Gender</label>
-            <div className='gender-inputs'>
-              <label htmlFor='man-gender-identity'>Man</label>
-              <input
-                id='man-gender-identity'
-                type='checkbox'
-                name='man-gender-identity'
-                required={true}
-                value='man'
-                onChange={handleChange}
-                checked={false}
-              />
-              <label htmlFor='woman-gender-identity'>Woman</label>
-              <input
-                id='woman-gender-identity'
-                type='checkbox'
-                name='woman-gender-identity'
-                required={true}
-                value='woman'
-                onChange={handleChange}
-                checked={false}
-              />
+            <div className='screen-md-right'>
+              <section>
+                <label htmlFor='url'>Profile picture</label>
+                {/* PICTURE.. */}
+                <input
+                  type='url'
+                  name='url'
+                  id='url'
+                  placeholder='Enter URL'
+                  required={true}
+                  value={formData.url}
+                  onChange={handleChange}
+                />
+                <div className='profile-img-container'>
+                  <img
+                    src={formData.url}
+                    alt='Profile Pic Preview'
+                    className='profile-img'
+                  />
+                </div>
+              </section>
             </div>
-          </section>
+          </div>
           <section>
-            <label htmlFor=''>Show me</label>
-            <div className='interest-label'>
-              <label htmlFor='man-gender-interest'>Man</label>
-              <input
-                id='man-gender-interest'
-                type='checkbox'
-                name='man-gender-interest'
-                required={true}
-                value='man'
-                onChange={handleChange}
-                checked={false}
-              />
-              <label htmlFor='woman-gender-interest'>Woman</label>
-              <input
-                id='woman-gender-interest'
-                type='checkbox'
-                name='woman-gender-interest'
-                required={true}
-                value='woman'
-                onChange={handleChange}
-                checked={false}
-              />
-            </div>
-          </section>
-
-          <section>
-            <label htmlFor='about'>About us(me)</label>
-            <input
-              id='about'
-              type='text'
-              name='about'
-              required={true}
-              placeholder='I like long wlks'
-              value={""}
-              onChange={handleChange}
-            />
-          </section>
-          <section>
-            <label htmlFor='url'>Profile picture</label>
-            {/* PICTURE.. */}
-            <input
-              type='url'
-              name='url'
-              id='url'
-              onChange={handleChange}
-              required={true}
-            />
+            <button type='submit' className='primary-btn form-submit-btn'>
+              submit
+            </button>
           </section>
         </form>
       </div>
