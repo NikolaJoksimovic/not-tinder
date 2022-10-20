ecd442888a29c609cde9b54414fd7585322785df
joksa19 <nikolajoksimovic95@gmail.com>
2022-10-20 11:11:05 +0200

    add loading with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import OnBoarding from "./pages/OnBoarding";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return (
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+  }, []);
+  return !loading ? (
+    <h1>Loading</h1>
+  ) : (
     <Router>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
